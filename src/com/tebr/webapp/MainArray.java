21713b6a551ff27b54d74481b33775bb8ceae4dd
tebr5923 <tebr5923@mail.ru>
2019-04-24 00:11:56 +0700

    lesson 2 Merge branch 'master' of https://github.com/tebr5923/basejava
    # Conflicts:
    #	src/ArrayStorage.java
    HW2 Ver.2

--- src/com/tebr/webapp/MainArray.java
+++ src/com/tebr/webapp/MainArray.java
@@ -1,9 +1,14 @@
+package com.tebr.webapp;
+
+import com.tebr.webapp.model.Resume;
+import com.tebr.webapp.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for com.tebr.webapp.storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -32,13 +37,13 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
                 case "update":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;
